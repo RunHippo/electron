245a8c4982dd984f62c93a14a6e6a5c236592dd0
See <276599239@qq.com>
2019-05-27 18:21:13 +0800

    similar match

--- src/assets/js/train.js
+++ src/assets/js/train.js
@@ -1,55 +1,20 @@
-
-import {
-    NerManager
-} from 'node-nlp';
-
 import fs from 'fs';
-const manager = new NerManager({
-    threshold: 0.8
-});
-fs.readFile(`${__dirname}/assets/standard.json`,function(err,data){
-    if(err){
-        return console.error(err);
-    }
-    const list = JSON.parse(data.toString());
-    list.forEach(element => {
-        console.log(element);
-        manager.addNamedEntityText(
-        'medical',
-        element.name,
-        ['zh'],
-        [element.name],
-        );
-        manager.findEntities(
-        '伤寒性脑膜炎'
-        ).then(entities => {
-            console.log(entities)
-        })
-        
-    });
-})
-class Mode{
-    constructor(){
-        fs.readFile(`${__dirname}/assets/standard.json`,function(err,data){
-            if(err){
-                return console.error(err);
-            }
-            const list = JSON.parse(data.toString());
-            list.forEach(element => {
-                console.log(element);
-                manager.addNamedEntityText(
-                'medical',
-                element.name,
-                ['zh'],
-                [element.name],
-                );
-                manager.findEntities(
-                '伤寒性脑膜炎'
-                ).then(entities => {
-                    console.log(entities)
-                })
-                
-            });
-        })
-    }
+import stringSimilarity from 'string-similarity';
+
+class Mode {
+  constructor() {
+    this.init = () => {
+      fs.readFile(`${__dirname}/assets/standard.json`, (err, data) => {
+        if (err) {
+          return console.error(err);
+        }
+        const list = JSON.parse(data.toString());
+        list.forEach((element) => {
+          this.list.push(element.name);
+        });
+        return false;
+      });
+    };
+    this.compare = name => stringSimilarity.findBestMatch(name, this.list);
+  }
 }
--- src/assets/js/xlsx.js
+++ src/assets/js/xlsx.js
@@ -1,23 +1,38 @@
-import xlsx from 'node-xlsx';
+import table from 'xlsx';
 import fs from 'fs';
+
 
 class Excel {
   constructor(files) {
     this.files = files;
-    this.getContent = () => {
-      this.workSheetsFromFile = xlsx.parse(fs.readFileSync(this.dir));
+    this.read = (fileList) => {
+      fileList.forEach((element) => {
+        const workExcels = table.readFile(element.path);
+        this.loadExcel(workExcels.Sheets);
+      });
     };
-    this.save = (fileList) => {
-      var s = [];
-      this.workSheetsFromFile = xlsx.parse(fs.readFileSync(fileList[0].path));
-      this.workSheetsFromFile[0].data.forEach(v=>{
-        var obj = {};
+    this.parseExcel = (fileList) => {
+      const s = [];
+      this.workSheetsFromFile = table.parse(fs.readFileSync(fileList[0].path));
+      this.workSheetsFromFile[0].data.forEach((v) => {
+        const obj = {};
         obj.code = v[0];
         obj.extraCode = v[1];
-        obj.name = v[2].replace(/\([\S\s]*\)/g,'').replace(/\（[\S\s]*\)/g,'').replace(/\([\S\s]*\）/g,'').replace(/\（[\S\s]*\）/g,'');
+        obj.name = v[2].replace(/\([\S\s]*\)/g, '').replace(/\（[\S\s]*\)/g, '').replace(/\([\S\s]*\）/g, '').replace(/\（[\S\s]*\）/g, '').replace(/\[[\S\s]*\]/g, '');
         s.push(obj);
-      })
+      });
       fs.writeFileSync('standard.json', JSON.stringify(s));
+    };
+    this.loadExcel = (sheets) => {
+      for (const excel in sheets) {
+        let excelIndex = 6;
+        for (const grid in sheets[excel]) {
+          if (sheets[excel][`X${excelIndex}`]) {
+            console.log(sheets[excel][`X${excelIndex}`].v);
+            excelIndex++;
+          }
+        }
+      }
     };
   }
 }
