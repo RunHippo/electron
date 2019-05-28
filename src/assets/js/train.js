e7716bf4659b80248468f747389e41d60badad25
See <276599239@qq.com>
2019-05-28 18:04:26 +0800

    excel output error

--- src/assets/js/train.js
+++ src/assets/js/train.js
@@ -3,18 +3,22 @@ import stringSimilarity from 'string-similarity';
 
 class Mode {
   constructor() {
-    this.init = () => {
-      fs.readFile(`${__dirname}/assets/standard.json`, (err, data) => {
-        if (err) {
-          return console.error(err);
-        }
-        const list = JSON.parse(data.toString());
-        list.forEach((element) => {
-          this.list.push(element.name);
-        });
-        return false;
+    this.list = [];
+    this.init();
+  }
+  init() {
+    fs.readFile(`${__dirname}/assets/standard.json`, (err, data) => {
+      if (err) {
+        return console.error(err);
+      }
+      const list = JSON.parse(data.toString());
+      list.forEach((element) => {
+        this.list.push(element.name);
       });
-    };
-    this.compare = name => stringSimilarity.findBestMatch(name, this.list);
+      return false;
+    });
+  }
+  find(name) {
+    return stringSimilarity.findBestMatch(name, this.list);
   }
 }
--- src/assets/js/xlsx.js
+++ src/assets/js/xlsx.js
@@ -1,38 +1,49 @@
 import table from 'xlsx';
 import fs from 'fs';
 
-
 class Excel {
-  constructor(files) {
+  constructor(files, mode) {
     this.files = files;
-    this.read = (fileList) => {
-      fileList.forEach((element) => {
-        const workExcels = table.readFile(element.path);
-        this.loadExcel(workExcels.Sheets);
-      });
-    };
-    this.parseExcel = (fileList) => {
-      const s = [];
-      this.workSheetsFromFile = table.parse(fs.readFileSync(fileList[0].path));
-      this.workSheetsFromFile[0].data.forEach((v) => {
-        const obj = {};
-        obj.code = v[0];
-        obj.extraCode = v[1];
-        obj.name = v[2].replace(/\([\S\s]*\)/g, '').replace(/\（[\S\s]*\)/g, '').replace(/\([\S\s]*\）/g, '').replace(/\（[\S\s]*\）/g, '').replace(/\[[\S\s]*\]/g, '');
-        s.push(obj);
-      });
-      fs.writeFileSync('standard.json', JSON.stringify(s));
-    };
-    this.loadExcel = (sheets) => {
-      for (const excel in sheets) {
+    this.mode = mode;
+  }
+  read() {
+    this.files.forEach((element) => {
+      const workExcels = table.readFile(element.path);
+      const insertHeader = ['match', 'accuracy'];
+      const insertData = [];
+      workExcels.SheetNames.forEach((sheetName) => {
         let excelIndex = 6;
-        for (const grid in sheets[excel]) {
-          if (sheets[excel][`X${excelIndex}`]) {
-            console.log(sheets[excel][`X${excelIndex}`].v);
-            excelIndex++;
+        // eslint-disable-next-line no-restricted-syntax
+        for (const grid in workExcels.Sheets[sheetName]) {
+          if (workExcels.Sheets[sheetName][`X${excelIndex}`]) {
+            const result = this.mode.find(workExcels.Sheets[sheetName][`X${excelIndex}`].v);
+            // workExcels.Sheets[sheetName][`X${excelIndex}`].v = `${result.bestMatch.target} rating:${result.bestMatch.rating}`;
+            insertData.push({
+              match: result.bestMatch.target,
+              accuracy: result.bestMatch.rating,
+            });
+            excelIndex += 1;
           }
         }
-      }
-    };
+      });
+      console.log(insertData);
+      table.utils.sheet_add_json(workExcels, insertData, {
+        header: insertHeader,
+        // origin: 'X5',
+      });
+      table.writeFile(workExcels, 'out.xlsx');
+    });
+  }
+  parseExcel(fileList) {
+    const s = [];
+    this.workSheetsFromFile = table.parse(fs.readFileSync(fileList[0].path));
+    this.workSheetsFromFile[0].data.forEach((v) => {
+      const obj = {};
+      obj.code = v[0];
+      obj.extraCode = v[1];
+      obj.name = v[2].replace(/\([\S\s]*\)/g, '').replace(/\（[\S\s]*\)/g, '').replace(/\([\S\s]*\）/g, '').replace(/\（[\S\s]*\）/g, '').replace(/\[[\S\s]*\]/g, '');
+      s.push(obj);
+    });
+    fs.writeFileSync('standard.json', JSON.stringify(s));
   }
 }
