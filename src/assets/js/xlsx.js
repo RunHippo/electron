32d09c462491a990cf236f5dfb0faa7b4cef25a5
seehe <seehe@mfcgd.com>
2019-09-18 18:45:54 +0800

    refactor

--- src/assets/js/xlsx.js
+++ src/assets/js/xlsx.js
@@ -13,38 +13,53 @@ class Excel {
         let excelIndex = 0;
         let signIndex = 0;
         let matchIndex = null;
-        const insertHeader = ['name','match', 'accuracy'];
+        let icdIndex = null;
+        let icdExcelIndex = 0;
+        const insertHeader = ['name', 'match', 'icd', 'accuracy'];
         const insertData = [];
         // eslint-disable-next-line no-restricted-syntax
         for (const grid in workExcels.Sheets[sheetName]) {
-          if(workExcels.Sheets[sheetName][grid].v==='疾病名称'&&!matchIndex){
+          if (workExcels.Sheets[sheetName][grid].v === '疾病名称' && !matchIndex) {
             matchIndex = grid.match(/[\D]*/g)[0];
-            excelIndex = parseInt(grid.match(/[\d]*/g)[1]);
+            excelIndex = parseInt(grid.match(/[\d]*/g)[1], 10);
             signIndex = excelIndex;
           }
-          if(grid.match(/[\D]*/g)[0]===matchIndex){
+          if (workExcels.Sheets[sheetName][grid].v === 'ICD-10编码' && !matchIndex) {
+            icdIndex = grid.match(/[\D]*/g)[0];
+            icdExcelIndex = parseInt(grid.match(/[\d]*/g)[1], 10);
+          }
+          if (grid.match(/[\D]*/g)[0] === icdIndex) {
+            let icdArrIndex = 0;
+            if (workExcels.Sheets[sheetName][icdIndex + icdExcelIndex]) {
+              insertData[icdArrIndex].icd = icdText;
+
+            }
+          }
+          if (grid.match(/[\D]*/g)[0] === matchIndex) {
             excelIndex += 1;
-            if (workExcels.Sheets[sheetName][matchIndex+excelIndex]) {
-              const word = workExcels.Sheets[sheetName][matchIndex+excelIndex].v;
+            if (workExcels.Sheets[sheetName][matchIndex + excelIndex]) {
+              const word = workExcels.Sheets[sheetName][matchIndex + excelIndex].v;
+              const icdText = workExcels.Sheets[sheetName][icdIndex + excelIndex].v;
               const result = this.mode.find(word);
               insertData.push({
                 name: word,
+                icd: icdText,
                 match: result.bestMatch.target,
                 accuracy: result.bestMatch.rating,
               });
-            }else{
+            } else {
               insertData.push({
                 name: '',
+                icd: '',
                 match: '',
                 accuracy: '',
               });
-
             }
           }
         }
         table.utils.sheet_add_json(workExcels.Sheets[sheetName], insertData, {
           header: insertHeader,
-          origin: 'BT'+ signIndex,
+          origin: 'BT' + signIndex,
         });
       });
       table.writeFile(workExcels, element.path);
